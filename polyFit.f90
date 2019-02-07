8b03d1c88849cd181312836fb82007b308e54f45
Sfjck-Linux VM <beokrus@gmail.com>
2019-02-07 04:57:18 -0800

    tester functions for VSD complete, moving onto LSS

---
+++ polyFit.f90
@@ -0,0 +1,88 @@
+!module containing code for polynomial fitting with SVD and LSS 
+!compile with:
+!gfortran -c -fdefault-real-8 polyFit.f90 -llapack
+
+module polyFit
+implicit none
+contains
+
+! fit function for single value decomp
+function yFit(x, y, numPoints, n, fitResults)
+	integer :: numPoints, n, a, i, j, k
+	real, dimension(:) :: x, y, fitResults
+	real, dimension(numPoints+1, n+1) :: b_a
+	real, dimension(n+1, n+1) :: b_ab, U, VT
+	real, dimension(n+1) :: fitParams, b, sig
+	real, dimension(numPoints+1) :: yFit
+	real :: chi2, DoF, condNum
+
+	! set up basis matrices
+	b_a = basis(numPoints, x, n) !want |y - b_a(x)|2 --> 0
+	b_ab = basisSOP(numPoints, b_a, n)
+	b = matmul(transpose(b_a), y) !solve b_ab*c = b
+
+	!solving using SVD
+	call svd(n+1, b_ab, U, sig, VT)
+	fitParams = svdFit(b, U, sig, VT) !=c_a
+
+	!calculate fit results
+	yFit = matmul(b_a, fitParams)
+	condNum = sig(1) / sig(n+1)
+	DoF = numPoints - (n+1)
+	chi2 = sum((y-yFit)**2) !std=1
+
+	!populate fit results to return
+	fitResults(1:n+1) = fitParams
+	fitResults(n+2) = condNum
+	fitResults(n+3) = chi2
+	fitResults(n+4) = DoF 
+end function
+
+! solve (U * sig * VT)x = b for x, using SVD
+function svdFit(b, U, sig, VT)
+	real :: b(:), U(:, :), sig(:), VT(:,:), svdFit(size(VT, 2))
+
+	svdFit = matmul(transpose(U), b) !UT*U = I; --> svdFit = UT*b = (sig*VT)x
+	where (sig > 10e-6) ! prevent div by 0
+		svdFit = svdFit / sig	!--> svdFit = UT*b/sig = VT*x
+	elsewhere
+		svdFit = 0.0
+	end where
+	svdFit = matmul(transpose(VT), svdFit) !VT*V = I; --> svdFit = V*UT*b/sig = x
+end function
+
+! use dgesvd to calculate U, sig, VT from MxN A. VT = V transposed, sig=diagonal of sigma
+subroutine svd(n, A, U, sig, VT)
+	integer :: n, LWork, info
+	real :: A(n,n), ACopy(n,n), U(n, n), sig(n), VT(n, n)
+	real, dimension(:), allocatable :: work
+
+	LWork = 8*n !must be at least 5*n
+	allocate(work(LWork))
+	ACopy = A !dgesvd destroys ACopy so A  is untouched
+	!DGESVD(JOBU, JOBVT, M, N, A, LDA, S, U, LDU, VT, LDVT, WORK, LWORK, INFO )
+	call dgesvd('A', 'A', n, n, ACopy, n, sig, U, n, VT, n, work, LWork, info)
+	if (info /= 0) call abort
+!	if (Work(1) /= LWork) write(*,*) "Optimal Lwork was", Work(1)
+end subroutine
+
+! basis is b_a(x)=cos(2pi*a*x)
+pure function basis(numPoints, x, n)
+	integer numPoints, n, a, i
+	real x(numPoints+1), basis(numPoints+1, n+1)
+	intent(in) numPoints, x, n
+	real, parameter :: pi = 3.14159265358979323846264338327950288419716939937510
+
+	forall (a=0:n, i=1:numPoints+1) basis(i, a+1) = cos(2*pi*a*x(i))
+end function
+
+! basisSOP is sum of product of b_a, b_b from eq3
+pure function basisSOP(numPoints, b_a, n)
+	integer numPoints, n, a, b
+	real b_a(numPoints+1, n+1), basisSOP(n+1, n+1)
+	intent(in) numPoints, b_a, n
+
+	forall (a=1:n+1, b=1:n+1) basisSOP(a,b) = sum(b_a(:, a)*b_a(:, b))
+end function
+
+end module
--- tester.f90
+++ tester.f90
@@ -1,44 +1,95 @@
 !compile with: 
 !gfortran -c -fdefault-real-8 tester.f90
 !link and run with:
-!gfortran tester.o polysvd.o -o tester && ./tester > output && cat output
+!gfortran tester.o polyFit.o -o tester && ./tester > output && cat output
 
 program tester
-use polysvd
+use polyFit
 implicit none
 
+real :: SVDTestA(3,3), SVDTestU(3,3), SVDTestSig(3), SVDTestVT(3,3)
+integer i, iost, numPoints
 integer, parameter :: maxPoints = 1000000 !arbitrarily large
 real xFile(maxPoints), yFile(maxPoints) !for reading the file
-integer i, iost, numPoints
 real, dimension(:), allocatable :: x, y !actual data arrays
-real, dimension(:), allocatable :: yFit3svd, yFit7svd, yFit3lss, yFit7lss
+real, dimension(:), allocatable :: yFit3svd, yFit7svd, yFit3lss, yFit7lss, fitResults
+!Write format labels:
+1 format(3f8.3)
+2 format(3f8.3,a,1f9.3,a,3f7.3)
+3 format(a,1f9.3,3f8.3)
+4 format(a,1f15.3)
+5 format(a,1f20.3)
+6 format(a,1f13.3)
+7 format(a,1f9.3,7f8.3)
 
-! read data from file "data.dat", at most n points
+!Problem 1:
+write(*,*) "Problem 1: Testing single value decomposition of matrix A(3x3):"
+SVDTestA = reshape((/1,2,3, 4,5,6, 7,8,9/), shape(SVDTestA))
+do i = 1,3
+	write(*,1) SVDTestA(i,:)
+end do
+call svd(3, SVDTestA, SVDTestU, SVDTestSig, SVDTestVT)
+write(*,*) "SVD results:     U(3x3)| Sigma(3)| VT(3x3):"
+do i = 1,3
+	write(*,2) SVDTestU(i,:), "|", SVDTestSig(i), "|", SVDTestVT(i,:)
+end do
+
+!Problem 2/3:
+write(*,*) ""
+write(*,*) "Problem 2&3: Fitting noisy data with SVD, n=3. Then computing fit parameters, condition number, and X2."
+
+! read data from file "data.dat", with limit maxPoints
 open(unit=1, file="data.dat", status="old", action="read")
 do i = 1, maxPoints
 	read (1, *, iostat=iost) xFile(i), yFile(i)
 	if (iost < 0) exit
 end do
 close(1)
-
 ! check actual data extent
 numPoints = i-1
 if (numPoints == maxPoints) write (0,*) "Read data extent was truncated, recompile with larger maxPoints"
-
 ! allocate data arrays
 allocate(x(numPoints), y(numPoints))
 x = xFile(1:i)
 y = yFile(1:i)
 
-!Problem 2
+! open file for writing
 open(unit=2, file="svd3.csv", status="replace", action="write")
-yFit3svd = yFitSVD(x, y, numPoints, n=3)
-!yFit7svd = yfitsvd(x, y, numPoints, n=7)
-write(2, *) "x, y, yFit3svd(x)"
+allocate(fitResults(3+1+3)) !3+1 for fit param; 3 for cond#/x2/dof
+yFit3svd = yFit(x, y, numPoints, 3, fitResults)
+write(2, *) "x, y, yFit3svd"
 do i = 1, numPoints
 	write (2, *) x(i), ",", y(i), ",", yFit3svd(i)
 end do
 close(2)
+write(*,*) "Fit complete, results printed csv to be plotted later"
+write(*,3) "SVD n=3 fit parameters:", fitResults(1:4)
+write(*,4) "Condition number:", fitResults(5) 
+write(*,5) "Chi-squared:", fitResults(6)
+write(*,6) "Degrees of freedom:", fitResults(7)
+write(*,*) "With the above results, the p-value of the chi-squared statistic is 0.991, making the fit very good."
+deallocate(fitResults)
 
-deallocate (x, y)
+!Problem 4:
+write(*,*) ""
+write(*,*) "Problem 4: Fitting noisy data with SVD, n=7. Then computing fit parameters, condition number, and X2."
+open(unit=2, file="svd7.csv", status="replace", action="write")
+allocate(fitResults(7+1+3)) !7+1 for fit param; 3 for cond#/x2/dof
+yFit7svd = yFit(x, y, numPoints, 7, fitResults)
+write(2, *) "x, y, yFit7svd"
+do i = 1, numPoints
+	write (2, *) x(i), ",", y(i), ",", yFit7svd(i)
+end do
+close(2)
+write(*,*) "Fit complete, results printed csv to be plotted later"
+write(*,7) "SVD n=7 fit parameters:", fitResults(1:8)
+write(*,4) "Condition number:", fitResults(9) 
+write(*,5) "Chi-squared:", fitResults(10)
+write(*,6) "Degrees of freedom:", fitResults(11)
+write(*,*) "With the above results, the p-value of the chi-squared statistic is 0.250, making the fit poorer. It is probably over-fitted."
+deallocate (x, y, fitResults)
+
+!Problem 5:
+write(*,*) ""
+wirte(*,*) "Problem 5: Fitting noisy data with LSS, n=3. Then computing fit parameters, condition number, and X2. Finally, comparing results to VSD.
 end program
