ef72fe0bb72751eeb6665c73c1cfb6a3558bf7fc
Sfjck-Linux VM <beokrus@gmail.com>
2019-03-05 12:17:56 -0800

    Prob 5 plotting complete, fit looks very good

--- Assignment3/runme.sh
+++ Assignment3/runme.sh
@@ -6,3 +6,10 @@ gfortran -c -fdefault-real-8 tester.f90 -llapack
 
 echo "running..."
 gfortran -g -llapack tester.o newtonMethod.o bracketSearch.o LMFit.o -o tester &&./tester > output && cat output
+
+echo "testing code complete"
+echo "plotting..."
+gnuplot Plot.gpl
+
+echo "all actions complete"
+echo "outputs can be found at 'Output' and 'Plot.pdf'"
